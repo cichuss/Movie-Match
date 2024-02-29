af02ec85d3645225a8afbfe027898a14b2cdff21
Hania111 <chococookimonster@gmail.com>
2024-02-29 10:36:43 +0000

    deleted on of the banners

--- header-logged-in/headerLogedIn.js
+++ header-logged-in/headerLogedIn.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('headerLogedIn.html')
+    fetch('../header-logged-in/headerLogedIn.html')
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML('afterbegin', data);
