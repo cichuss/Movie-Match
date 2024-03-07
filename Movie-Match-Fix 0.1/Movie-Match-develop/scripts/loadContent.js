927c68269a72cdebdb031a66387efd9cb4669bf0
Roberbart <72571086+Roberbart@users.noreply.github.com>
2024-03-07 05:07:40 +0000

    Add temp fix of Glitch
    -Fixed HTML format of contentLoader
    -Re-shaped the index
    -...Etc

---
+++ Movie-Match-Fix 0.1/Movie-Match-develop/scripts/loadContent.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', init);
+function init() {
+    loadContent('../home-page/home1.html', 'home-container');
+    loadContent('../menu/menu.html', 'menu');
+    loadContent('../header/header.html', 'header');
+    loadContent('../footer/footer.html', 'footer');
+}
+
+function loadContent(url, targetId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(targetId).innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Wystąpił błąd podczas wczytywania zawartości:', error);
+        });
+}
---
+++ Movie-Match-Fix 0.1/Movie-Match-develop/scripts/navigation.js
@@ -0,0 +1,31 @@
+function redirectToIndex() {
+    window.location.href = '../index/index.html';
+}
+
+function redirectToLogin() {
+    window.location.href = '../login-page/login.html';
+}
+
+function redirectToLiked() {
+    loadContent('../liked-movies-page/liked.html', 'home-container');
+}
+
+function redirectToTrueIndex() {
+    window.location.href = '../home-page/home.html';
+}
+function redirectToHome() {
+    loadContent('../home-page/home2.html', 'home-container');
+}
+
+function redirectToShake() {
+    loadContent('../shake-page/shake.html', 'home-container');
+}
+
+function redirectToConnect() {
+    loadContent('../connect-page/connect.html', 'home-container');
+}
+function redirectToRegister() {
+    window.location.href = '../register-page/register.html';
+}
+
+
